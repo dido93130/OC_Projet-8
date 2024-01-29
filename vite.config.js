94bf448ba2c185601dcb9530853c761bacea9739
Eddy Capirchio <98024166+dido93130@users.noreply.github.com>
2024-01-29 16:32:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./OC_Projet-8",
+  base: "/OC_Projet-8",
   plugins: [ react()],
   server: {
     port: 3001,

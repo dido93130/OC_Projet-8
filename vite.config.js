3675a7e7f89c1ce92171d8cd968f1b5460334066
Eddy Capirchio <dido93430@hotmail.fr>
2024-01-29 16:13:30 +0100

    Projet8 final

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ react()],
+  base: "/OC_Projet-8",
   server: {
     port: 3001,
   },
